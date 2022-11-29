970179f1236417bfee7e68b6c8a24aa10efc892d
hiroshi <hiroshi.tan57@gmail.com>
2022-11-29 20:36:13 +0000

    Add Dockerfile & workflows action

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import config from './config/configuration';
+
+const PORT = config().port;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:3000', 'https://showcase.dev.lab.eten.bible'],
   });
-  await app.listen(3001);
+  await app.listen(PORT);
 }
 bootstrap();
